a34f249af2f9fdd160c953c9221571b6bea9fb1e
mocia <mo_cia@yahoo.com>
2018-09-26 10:25:43 +0700

    coba UI

--- src/routes/belajar.js
+++ src/routes/belajar.js
@@ -11,6 +11,20 @@ module.exports = [
             permission: { "C9": 1, "A2": 1 },
             iconClass: 'fa fa-kaaba'
         }
+    },
+
+    {
+        route: 'sinaus',
+        name: 'sinaus',
+        moduleId: './modules/belajar/sinau/index',
+        nav: true,
+        title: 'Sinau',
+        auth: true,
+        settings: {
+            group: "belajar",
+            permission: { "C9": 1, "A2": 1 },
+            iconClass: 'fa fa-kaaba'
+        }
     }
 ]
 
